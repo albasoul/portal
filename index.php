a930fd773489cb91068cc71c7ae6a0d2a3f17b01
Blendi Gashi <blendi.gashi@gmail.com>
2014-03-14 19:26:19 +0100

    besfortii

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	/*
 	* Kontrollon nese faqja eshte e lejuar te kete qasje nga Administratori
 	*/
-	// ta qifsha komentin ?? :p
+	// o Besfort o bukurosh  : BESFORT + GITHUB = <3
 	if($page->isActivated()){
 		/*
 		* 	Kontrollon nese studenti eshte i kyqur permes funksionit loggedIn();
