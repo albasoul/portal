dbbacfe383265dc12d841833c416bab25ec6024b
Blendi Gashi <blendi.gashi@gmail.com>
2014-03-14 18:06:55 +0100

    ndryshim i vogel te studenti

--- classes/studenti.class.php
+++ classes/studenti.class.php
@@ -14,11 +14,11 @@ class Studenti{
 		global $lidhja;
 		$studentQuery = $lidhja->query("SELECT emri,mbiemri,email,lendet_kaluara,lokacioni,foto FROM studentet WHERE SID=$sid LIMIT 1");
 		if($studentQuery->num_rows == 1){
-			$student = $studentQuery->fetch_assoc();
-		$this->emri = $student['emri'];
-		$this->mbiemri = $student['mbiemri'];
-		$this->email = $student['email'];
-		$this->lendet_kaluara = explode(",", $student['lendet_kaluara']);
+			$student = $studentQuery->fetch_assoc(); // marrja e informacioneve dhe ruajtja ne $student
+			$this->emri = $student['emri'];
+			$this->mbiemri = $student['mbiemri'];
+			$this->email = $student['email'];
+			$this->lendet_kaluara = explode(",", $student['lendet_kaluara']); // ndarja e ID te lendeve te kaluara ne nje Array 
 		}
 	}
 	function getEmri(){
