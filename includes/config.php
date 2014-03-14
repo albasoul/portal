8107c693c26e7b68ddd91c50dd54603a4b4aae97
Blendi Gashi <blendi.gashi@gmail.com>
2014-03-14 15:25:13 +0100

    u shtua lidhja ne databas

--- includes/config.php
+++ includes/config.php
@@ -4,5 +4,6 @@
 	# Lidhja me database
 	# dhe informacione tjera themelore si session_start, cookie ,etj
 	#
-
+	
+	$lidhja = new mysqli('localhost','portal_user','portal123','portal') or die('Nuk mund te lidhet me databas!');
 ?>
