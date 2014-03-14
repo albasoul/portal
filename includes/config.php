f8432ebe7f6b6202693adbc3cb31478e9bf453ab
Blendi Gashi <blendi.gashi@gmail.com>
2014-03-14 15:39:12 +0100

    perfshihet funksionet.php dhe shtohet session_start

--- includes/config.php
+++ includes/config.php
@@ -8,5 +8,9 @@
 	$lidhja = new mysqli('localhost','portal_user','K5rAp9NeJS3MLb4b','portal');
 	if ($lidhja->connect_error) {
 		echo 'Kontrolloni databasen.';
+		die();
 	}
+	session_start();
+	include('funksionet.php');
+
 ?>
