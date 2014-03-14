05c7928de4770b72bfcaf9928fe1f03e637f990f
Blendi Gashi <blendi.gashi@gmail.com>
2014-03-14 18:19:55 +0100

    ndryshime baze

--- classes/studenti.class.php
+++ classes/studenti.class.php
@@ -4,12 +4,17 @@
 *	Te gjitha funksionet per nje student gjinden ketu
 */
 class Studenti{
-	private $emri;
-	private $mbiemri;
-	private $email;
-	private $lendet_kaluara;
-	private $lokacioni;
-	private $foto;
+	private $emri = "panjohur";
+	private $mbiemri = "panjohur";
+	private $email = "email@email.com";
+	private $lendet_kaluara = array();
+	private $lokacioni = "panjohur";
+	private $foto = "img/studente/profil/default.png";
+
+	/*
+	* 	Krijon objetkin Studenti ne varesi nga $sid,
+	*	nese nuk ka $sid atehere mirren vlerat Default
+	*/
 	function __construct($sid){
 		global $lidhja;
 		$studentQuery = $lidhja->query("SELECT emri,mbiemri,email,lendet_kaluara,lokacioni,foto FROM studentet WHERE SID=$sid LIMIT 1");
@@ -19,16 +24,45 @@ class Studenti{
 			$this->mbiemri = $student['mbiemri'];
 			$this->email = $student['email'];
 			$this->lendet_kaluara = explode(",", $student['lendet_kaluara']); // ndarja e ID te lendeve te kaluara ne nje Array 
+			$this->lokacioni = $student['lokacioni'];
+			$this->foto = $student['foto'];
 		}
 	}
+	/*
+	* 	Kthen emrin e studentit
+	*/
 	function getEmri(){
-
+		return $this->emri;
 	}
+	/*
+	* 	Kthen mbiemrin e studentit
+	*/
 	function getMbiemri(){
-
+		return $this->mbiemri;
 	}
+	/*
+	* 	Kthen lokacionin e fotografise se profilit te studentit
+	*/
 	function getFoto(){
-
+		return $this->foto;
+	}
+	/*
+	* 	Kthen lokacionin e studentit
+	*/
+	function getLokacioni(){
+		return $this->lokacioni;
+	}
+	/*
+	* 	Kthen emailin e studentit
+	*/
+	function getEmail(){
+		return $this->email;
+	}
+	/*
+	* 	Kthen ID e te gjitha lendeve te cilat i ka kaluar studenti
+	*/
+	function getLendetKaluara(){
+		return $this->lendet_kaluara;
 	}
 	function loggedIn(){
 		if(!empty($_SESSION['logged_in']) && $_SESSION['logged_in'] === "loggedIn")
--- index.php
+++ index.php
@@ -2,14 +2,17 @@
 	# perfshije config.php
 	include('includes/config.php');
 	$page = new Page();
+	if(empty($_SESSION['s_id'])){
+		$_SESSION['s_id']=0;
+	}
 	$studenti = new Studenti($_SESSION['s_id']);
 	if($page->isActivated()){
-		#if($studenti->loggedIn()){
+		if($studenti->loggedIn()){
 			$page->showPage();
-		#}
-		#else{
-		#	$page->showLoggIn();
-		#}
+		}
+		else{
+			$page->showLoggIn();
+		}
 	}
 	else{
 		$page->hidePage();
