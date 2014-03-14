9c6c827603242315fe8e0db8cf124ee380c091f2
Blendi Gashi <blendi.gashi@gmail.com>
2014-03-14 15:33:04 +0100

    ndrrim psw

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@
 	# dhe informacione tjera themelore si session_start, cookie ,etj
 	#
 	
-	$lidhja = new mysqli('localhost','portal_user','portal123','portal');
+	$lidhja = new mysqli('localhost','portal_user','K5rAp9NeJS3MLb4b','portal');
 	if ($lidhja->connect_error) {
 		echo 'Kontrolloni databasen.';
 	}
