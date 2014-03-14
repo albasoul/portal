893f569a9b1f88475130f7abfbb964e844865a98
Blendi Gashi <blendi.gashi@gmail.com>
2014-03-14 23:13:28 +0100

    krijimi i klases Lajmi

---
+++ classes/lajmi.class.php
@@ -0,0 +1,10 @@
+<?php
+class Lajmi{
+	private $titulli = "panjohur";
+	private $pershkrimi = "panjohur";
+	private $body = "panjohur";
+	function __construct(){
+		$this->titulli= "test";
+	}
+}
+?>
