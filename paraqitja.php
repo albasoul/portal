41ee2339ddbabf7b835293419aaf2a2251229391
Blendi Gashi <blendi.gashi@gmail.com>
2014-04-07 03:14:55 +0200

    if.

--- paraqitja.php
+++ paraqitja.php
@@ -7,12 +7,24 @@ if(!empty($_POST['LID']) AND is_numeric($_POST['LID']) AND !empty($_POST['profes
 
 	$profat = $lenda->getProfID(); // ID e profesorave te asaj lende
 
-	if(in_array($prof->getID(), $profat)){ // kontrollojm nese ID e dhene eshte profesor i asaj lende te caktuar
-		echo 'PO';
-	}
-	else{
-		echo 'JO';
-	}
+		if($lenda->getDrejtimi() == $studenti->getDrejtimi()){
+			if(!$studenti->getLendaKaluara($lenda->getID())){
+				if(in_array($prof->getID(), $profat)){ // kontrollojm nese ID e dhene eshte profesor i asaj lende te caktuar
+					echo 'PO';
+				}
+				else{
+					echo 'JO';
+				}
+			}
+			else{
+				header('Location: index.php?faqja=paraqitja&mesazhi=paraqitja1');
+				die();
+			}
+		}
+		else{
+			header('Location: index.php?faqja=paraqitja&mesazhi=paraqitja1');
+			die();
+		}
 }
 else{
 	header('Location: index.php?faqja=paraqitja');
